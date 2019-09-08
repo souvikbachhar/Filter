58b3bc3494ca7a1e984d34fad82c0977558a72d9
Souvik Bachhar <Souvik Bachhar@Souvik>
2019-09-08 15:17:02 +0530

    https certificate

--- Assignment1/src/main/java/com/souvik/controller/MyController.java
+++ Assignment1/src/main/java/com/souvik/controller/MyController.java
@@ -1,9 +1,13 @@
 package com.souvik.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.souvik.vo.UserProfile;
@@ -29,5 +33,11 @@ public class MyController {
 	public  String sayName(){
 		return "Bank Name: ICICI";
 	}
+	
+    @RequestMapping(value = "/secured", method = RequestMethod.GET)
+    public String secured(){
+        System.out.println("Inside secured()");
+        return "Hello user !!! : " + new Date();
+    }
 
 }
